3e33eb4ee992c455e78cea447a8c1d769fc5334e
SRIRAM <srirammurugesan1807@gmail.com>
2025-07-22 19:09:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,28 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+  const prices = document.querySelectorAll(".price");
+  let total = 0;
+  prices.forEach(priceEl => {
+    total += Number(priceEl.innerText);
+  });
+
+  const table = document.querySelector("table");
+  const existingTotalRow = document.querySelector(".total-row");
+  if (existingTotalRow) {
+    existingTotalRow.remove();
+  }
+
+  const totalRow = document.createElement("tr");
+  totalRow.classList.add("total-row");
+
+  const totalCell = document.createElement("td");
+  totalCell.colSpan = 2;
+  totalCell.style.fontWeight = "bold";
+  totalCell.innerText = `Total Price: Rs ${total}`;
+
+  totalRow.appendChild(totalCell);
+  table.appendChild(totalRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
